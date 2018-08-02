98b3c929ad8aea5bcd97d9827ce290706ab7154a
rob <rschlub@gmail.com>
2018-08-02 15:54:30 -0700

    update to make it easier to update packages

--- start_env.sh
+++ start_env.sh
@@ -1,26 +1,33 @@
 PROJECT_PATH=`pwd`
-PROJECT_NAME=dev
+PROJECT_NAME=nodepy
+BUILD=dev
 
 if [ $1 ];
 then
-  PROJECT_NAME=$1
+  BUILD=$1
 fi
 
-cp $PROJECT_NAME/Dockerfile Dockerfile
+cp $BUILD/Dockerfile Dockerfile
 
-docker build -t $DOCKER_HUB_USERNAME/$PROJECT_NAME .
 
-rm Dockerfile
 
-if [ $PROJECT_NAME = 'base' ];
+if [ $BUILD = 'base' ];
 then
-  docker run -it --rm --name nodepy-base $DOCKER_HUB_USERNAME/nodepy
+  docker build -t $DOCKER_HUB_USERNAME/$PROJECT_NAME .
+  rm Dockerfile
+  docker run -it --rm --name nodepy-base $DOCKER_HUB_USERNAME/$PROJECT_NAME
 fi
-if [ $PROJECT_NAME = 'dev' ];
+if [ $BUILD = 'dev' ];
 then
+  rm temp/package*
+  docker build -t $DOCKER_HUB_USERNAME/$PROJECT_NAME:$BUILD .
+  rm Dockerfile
   docker run -it --rm \
-    -v $PROJECT_PATH/dev/package.json:/opt/app/package.json \
-    -v $PROJECT_PATH/dev/package-lock.json:/opt/app/package-lock.json \
+    -v $PROJECT_PATH/temp:/opt/app/temp \
     --name nodepy-base \
-    $DOCKER_HUB_USERNAME/nodepy:dev
+    $DOCKER_HUB_USERNAME/$PROJECT_NAME:$BUILD
 fi
+
+
+# -v $PROJECT_PATH/dev/package.json:/opt/app/package.json \
+#     -v $PROJECT_PATH/dev/package-lock.json:/opt/app/package-lock.json \
